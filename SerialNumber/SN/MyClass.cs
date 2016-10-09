e62c73165e30bf55089967d7c867483bbca37af0
Ofer Haviv <oferhaviv@gmail.com>
2016-10-09 22:02:35 +0300

    first

---
+++ SerialNumber/SN/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Lia
+ * Date: 09/10/2016
+ * Time: 21:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace SN
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
---
+++ SerialNumber/SerialNumber/MainForm.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Lia
+ * Date: 09/10/2016
+ * Time: 21:47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SerialNumber
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			
+			//debug
+			Int64 seed =0;
+			
+			seed ++;
+			
+			
+		}
+		private string uname2seed(string uname)
+		{
+			return null;
+		}
+	}
+}
